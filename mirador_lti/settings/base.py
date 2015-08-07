45d932850ec7913c260f6b4ea42b89cbcd31e0e8
Arthur Barrett <abarrett@fas.harvard.edu>
2015-08-07 17:23:54 -0400

    added allowed hosts

--- mirador_lti/settings/base.py
+++ mirador_lti/settings/base.py
@@ -26,7 +26,7 @@ SECRET_KEY = SECURE_SETTINGS.get('django_secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.harvard.edu', 'localhost']
 
 
 # Application definition
