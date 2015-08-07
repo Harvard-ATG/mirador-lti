da09b3f87ca12b591294914003ac55d4a3f1029c
Arthur Barrett <abarrett@fas.harvard.edu>
2015-08-07 15:07:50 -0400

    Added script to load image metadata from S3 bucket into local DB.

--- mirador/admin.py
+++ mirador/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import IsiteImages, LTICourseImages
 
 class IsiteImagesAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'isite_keyword', 'isite_topic_id','isite_file_name', 'isite_file_url', 'isite_file_title', 'isite_file_description', 's3_bucket', 's3_key', 'created')
 
 class LTICourseImagesAdmin(admin.ModelAdmin):
     pass
