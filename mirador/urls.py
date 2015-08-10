db2ee0ae5eb1537fa162ef405484baafe7666a9c
Arthur Barrett <abarrett@edx.org>
2015-08-09 21:09:41 -0400

    Fixed missing logo in mirador.

--- mirador/urls.py
+++ mirador/urls.py
@@ -1,5 +1,12 @@
+from django.conf import settings
 from django.conf.urls import url
+from django.views.static import serve
 from .views import index
+import os
+
 urlpatterns = [
-    url(r'^(?P<course_id>\d+)$', index, name='index')
+    url(r'^(?P<course_id>\d+)$', index, name='index'),
+    url(r'^images/logos/(?P<path>.*)$', serve, { 
+        'document_root': os.path.join(settings.STATIC_ROOT, 'images', 'logos')
+    }),
 ]
