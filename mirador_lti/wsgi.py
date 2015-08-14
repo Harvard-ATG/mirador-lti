b5777817500aac5a0b8ec0b29560c8f6bf165469
Arthur Barrett <abarrett@edx.org>
2015-08-13 22:37:41 -0400

    Configuring for heroku deploymen

--- mirador_lti/wsgi.py
+++ mirador_lti/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mirador_lti.settings")
+if os.environ.get("DJANGO_SETTINGS_MODULE", None) == "mirador_lti.settings.heroku":
+    application = Cling(get_wsgi_application())
+else:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mirador_lti.settings.aws")
+    application = get_wsgi_application()
 
-application = get_wsgi_application()
