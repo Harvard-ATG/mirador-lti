196de987fba526592a7229ee3b6d2ce0c408f860
Arthur Barrett <abarrett@fas.harvard.edu>
2015-08-21 16:24:18 -0400

    Updated aws settings.

--- mirador_lti/settings/aws.py
+++ mirador_lti/settings/aws.py
@@ -4,9 +4,36 @@ from .secure import SECURE_SETTINGS
 ALLOWED_HOSTS = ['.harvard.edu', 'localhost']
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': SECURE_SETTINGS.get('db_default_name', 'canvas_syllabus_export'),
+        'USER': SECURE_SETTINGS.get('db_default_user', 'canvas_syllabus_export'),
+        'PASSWORD': SECURE_SETTINGS.get('db_default_password'),
+        'HOST': SECURE_SETTINGS.get('db_default_host', '127.0.0.1'),
+        'PORT': SECURE_SETTINGS.get('db_default_port', 5432),
+    } 
+}
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 SESSION_COOKIE_SECURE = True
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
+REDIS_HOST = SECURE_SETTINGS.get('redis_host', '127.0.0.1')
+REDIS_PORT = SECURE_SETTINGS.get('redis_port', 6379)
+
+CACHES = {
+    'default': {
+        'BACKEND': 'redis_cache.RedisCache',
+        'LOCATION': "%s:%s" % (REDIS_HOST, REDIS_PORT),
+        'KEY_PREFIX': 'canvas_syllabus_export', # Provide a unique value for shared cache
+        'TIMEOUT': SECURE_SETTINGS.get('cache_timeout_in_secs', 60 * 20),
+    },
+}
+
 SECRET_KEY = SECURE_SETTINGS.get('django_secret_key')
+
 LTI_OAUTH_CREDENTIALS = SECURE_SETTINGS.get('lti_oauth_credentials', {})
+
 IIIF_IMAGE_SERVER_URL = SECURE_SETTINGS.get('iiif_image_server_url', 'http://localhost:8000/loris/')
