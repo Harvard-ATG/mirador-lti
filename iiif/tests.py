b1cbf4fe7fac4618bf35c459ab432b3f5cb453b7
Arthur Barrett <abarrett@fas.harvard.edu>
2015-09-17 16:19:36 -0400

    added more tests to iiif.tests

--- iiif/tests.py
+++ iiif/tests.py
@@ -10,8 +10,7 @@ class IIIFManifestTest(unittest.TestCase):
 
     def create_manifest(self, manifest_id, **kwargs):
         request = self.factory.get(reverse('iiif:manifest', kwargs={"manifest_id":manifest_id, "object_type": "manifest"}))
-        manifest = IIIFManifest(request, manifest_id, **kwargs)
-        return (request, manifest)
+        return (request, IIIFManifest(request, manifest_id, **kwargs))
 
     def create_images_list(self):
         images = [
@@ -22,19 +21,33 @@ class IIIFManifestTest(unittest.TestCase):
         return images
 
     def test_create_manifest_with_images(self):
-        manifest_id = 1
-        request, manifest = self.create_manifest(manifest_id)
+        request, manifest = self.create_manifest(1)
         images = self.create_images_list()
-        non_link_images = [img for img in images if img['is_link'] is False]
         manifest.create(images=images)
         md = manifest.to_dict()
-
-        for key in ('@context', '@type', '@id', 'label', 'description', 'sequences'):
-            self.assertTrue(key in md, "manifest should have a %s attribute" % key)
+        
+        non_link_images = [img for img in images if img['is_link'] is False]
 
         self.assertEqual(len(md['sequences']), 1, "should have one default sequence")
         self.assertEqual(len(md['sequences'][0]['canvases']), len(non_link_images), "one canvas per image")
-
         for idx, canvas in enumerate(md['sequences'][0]['canvases']): 
             self.assertEqual(canvas['label'], non_link_images[idx]['label'], "canvas label should match image label")
+
+    def test_manifest_attributes(self):
+        label = 'test label'
+        description = 'test description'
+        request, manifest = self.create_manifest(1, label=label, description=description)
+        md = manifest.to_dict()
         
+        expected_attr = {
+            "@context": "http://iiif.io/api/presentation/2/context.json",
+            "@type": "sc:Manifest",
+            "@id": request.build_absolute_uri(),
+            "label": label,
+            "description": description,
+            "sequences": []
+        }
+        
+        for key in expected_attr.keys():
+            self.assertTrue(key in md, "manifest should have a %s attribute" % key)
+            self.assertEqual(md[key], expected_attr[key])
